90c489e899255fd86f8e22910928aca75d3c1154
Andrew Bender <andrew.bender710@gmail.com>
2022-06-10 13:38:18 -0700

    Remove trials with bad sparam model fits and set all remaining NaNs to zero

--- code/process_data.py
+++ code/process_data.py
@@ -219,7 +219,19 @@ def convert_sparam_df_to_mne(tfr_mt, sparam_df, save_fname):
     sparam_df = sparam_df.set_index(['trial', 'channel', 'timepoint'])
     if len(sparam_df) != np.prod(sparam_df.index.levshape):
         sparam_df = sparam_df.reindex(pd.MultiIndex.from_product(
-            [np.arange(n) for n in sparam_df.index.levshape]))
+            [np.arange(n) for n in sparam_df.index.levshape],
+            names=['trial', 'channel', 'timepoint']))
+
+    # Remove trials with any failed fits
+    sparam_df_flat = sparam_df.reset_index()
+    failed_model_fit_trials = sparam_df.loc[sparam_df.isna().all(
+        axis=1)].reset_index()['trial'].unique()
+    sparam_df = sparam_df_flat[~sparam_df_flat['trial'].isin(
+        failed_model_fit_trials)].set_index(['trial', 'channel', 'timepoint'])
+    print(sparam_df.loc[sparam_df.isna().all(axis=1)].shape)
+    # Fill remaining NaNs with zero
+    sparam_df = sparam_df.fillna(0)
+    print(sparam_df.isna().sum())
 
     # Collapse frequencies of TFR
     info = tfr_mt.copy().average(dim='freqs').info
