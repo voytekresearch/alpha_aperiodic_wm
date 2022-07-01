66604e2d541941480e946184b9e73a830a28c994
Andrew Bender <andrew.bender710@gmail.com>
2022-06-30 17:49:38 -0700

    Put total power and sparam params in same CTF slope plot

--- code/spec_param_iem.py
+++ code/spec_param_iem.py
@@ -8,6 +8,10 @@ import params
 from train_and_test_iem import plot_ctf_slope, train_and_test_all_subjs
 
 if __name__ == '__main__':
+    # Reproduce total alpha power IEM
+    tot_pw_channel_offsets, tot_pw_ctf_slopes, t_arr = train_and_test_all_subjs(
+        'total_power')
+
     # Decode spatial location from alpha oscillatory power
     pw_channel_offsets, pw_ctf_slopes, _ = train_and_test_all_subjs('PW')
 
@@ -19,5 +23,7 @@ if __name__ == '__main__':
     # model
     ctf_slopes_fname = os.path.join(params.FIG_DIR, 'ctf_slopes')
     ctf_slopes = {
-        'Power': pw_ctf_slopes, 'Exponent': exp_ctf_slopes}
+        'Alpha total power': tot_pw_ctf_slopes,
+        'Alpha oscillatory power': pw_ctf_slopes,
+        'Aperiodic exponent': exp_ctf_slopes}
     plot_ctf_slope(ctf_slopes, t_arr, save_fname=ctf_slopes_fname)
