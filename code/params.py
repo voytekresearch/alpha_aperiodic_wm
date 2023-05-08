77a721024e3652ab7f4a7b178bbd07386758dbac
Andrew Bender <andrew.bender710@gmail.com>
2023-05-08 14:31:54 -0700

    Process data in lab storage on cluster rather than individual home directory

--- code/params.py
+++ code/params.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 # Directories
-DATA_DIR = '/home/AD/abender/decoding_spatial_wm/data'
+DATA_DIR = '/pentagon/bvoyteklab/decoding_spatial_wm/data'
 DOWNLOAD_DIR = f'{DATA_DIR}/by_experiment'
 PROCESSED_DIR = f'{DATA_DIR}/by_subject'
 TOTAL_POWER_DIR = f'{DATA_DIR}/total_power'
