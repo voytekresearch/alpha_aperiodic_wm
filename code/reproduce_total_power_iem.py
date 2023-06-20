99f3ca92864ec58b1531c0240c9673cb16ead71c
Andrew Bender <andrew.bender710@gmail.com>
2023-06-20 09:13:29 -0700

    Remove separate total power reproduction script

--- code/reproduce_total_power_iem.py
+++
@@ -1,9 +0,0 @@
-"""Reproduce decoding of spatial location from total alpha power using an IEM by
-Foster and colleagues (https://pubmed.ncbi.nlm.nih.gov/26467522/)."""
-
-# Import neccesary modules
-from train_and_test_iem import train_and_test_all_subjs
-
-if __name__ == '__main__':
-    # Reproduce total alpha power IEM
-    train_and_test_all_subjs('total_power')
