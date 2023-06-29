69eb06cbafc434f15e273d3055d93a8de8a8d1d7
Andrew Bender <andrew.bender710@gmail.com>
2023-06-29 16:18:22 -0700

    Update cluster shell scripts

--- code/process_data_cluster.sh
+++ code/process_data_cluster.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=68
+#SBATCH --cpus-per-task=72
 #SBATCH --mem-per-cpu=2G
 #SBATCH --output=process_data_cluster.out
 #SBATCH --mail-type=ALL
@@ -14,46 +14,46 @@
 cd /home/AD/abender/decoding_spatial_wm
 source .env/bin/activate
 
-# Obtain head node IP address
-nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
+# ===== DO NOT CHANGE THINGS HERE UNLESS YOU KNOW WHAT YOU ARE DOING =====
+# This script is taken from the slurm-template.sh script in the Ray repo:
+# https://docs.ray.io/en/latest/cluster/vms/user-guides/community/slurm-template.html#slurm-template
+redis_password=$(uuidgen)
+export redis_password
+
+nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST") # Getting the node names
 nodes_array=($nodes)
 
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+node_1=${nodes_array[0]}
+ip=$(srun --nodes=1 --ntasks=1 -w "$node_1" hostname --ip-address) # making redis-address
 
-# If we detect a space character in the head node IP, we'll
+# if we detect a space character in the head node IP, we'll
 # convert it to an ipv4 address. This step is optional.
-if [[ "$head_node_ip" == *" "* ]]; then
-IFS=' ' read -ra ADDR <<<"$head_node_ip"
-if [[ ${#ADDR[0]} -gt 16 ]]; then
-  head_node_ip=${ADDR[1]}
-else
-  head_node_ip=${ADDR[0]}
-fi
-echo "IPV6 address detected. We split the IPV4 address as $head_node_ip"
+if [[ "$ip" == *" "* ]]; then
+  IFS=' ' read -ra ADDR <<< "$ip"
+  if [[ ${#ADDR[0]} -gt 16 ]]; then
+    ip=${ADDR[1]}
+  else
+    ip=${ADDR[0]}
+  fi
+  echo "IPV6 address detected. We split the IPV4 address as $ip"
 fi
 
-# Start the Ray head node
 port=6379
-ip_head=$head_node_ip:$port
+ip_head=$ip:$port
 export ip_head
 echo "IP Head: $ip_head"
 
-echo "Starting HEAD at $head_node"
-srun --nodes=1 --ntasks=1 -w "$head_node" \
-    ray start --head --node-ip-address="$head_node_ip" --port=$port \
-    --num-cpus "${SLURM_CPUS_PER_TASK}" --block &
+echo "STARTING HEAD at $node_1"
+srun --nodes=1 --ntasks=1 -w "$node_1" \
+  ray start --head --node-ip-address="$ip" --port=$port --redis-password="$redis_password" --block &
+sleep 30
 
-# Start the Ray worker nodes
-worker_num=$((SLURM_JOB_NUM_NODES - 1))
-
+worker_num=$((SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
 for ((i = 1; i <= worker_num; i++)); do
-    node_i=${nodes_array[$i]}
-    echo "Starting WORKER $i at $node_i"
-    srun --nodes=1 --ntasks=1 -w "$node_i" \
-        ray start --address "$ip_head" \
-        --num-cpus "${SLURM_CPUS_PER_TASK}" --block &
-    sleep 5
+  node_i=${nodes_array[$i]}
+  echo "STARTING WORKER $i at $node_i"
+  srun --nodes=1 --ntasks=1 -w "$node_i" ray start --address "$ip_head" --redis-password="$redis_password" --block &
+  sleep 5
 done
 
 # Call Python script
