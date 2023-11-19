0b033406c06968cc5bfaa5503247929f76e5c595
Andrew Bender <andrew.bender710@gmail.com>
2023-11-19 14:07:28 -0800

    Fix PEP issues

--- code/spec_param_iem.py
+++ code/spec_param_iem.py
@@ -4,9 +4,9 @@ using same inverted encoding model (IEM) and EEG data as Foster and colleagues
 
 # Import neccesary modules
 import os
+import time
 import matplotlib.pyplot as plt
 import params
-import time
 from train_and_test_iem import plot_ctf_slope, plot_ctf_slope_paired_ttest, \
     train_and_test_all_subjs
 
@@ -31,17 +31,17 @@ def fit_iem_all_params(
     for sp_param in sp_params:
         # Start timer
         start = time.time()
-        _, ctf_slopes, ctf_slopes_null, t_arrays = train_and_test_all_subjs(
-            sp_param, sparam_dir)
-        ctf_slopes_all_params[sp_param] = ctf_slopes
-        ctf_slopes_null_all_params[sp_param] = ctf_slopes_null
+        _, ctf_slopes_one_param, ctf_slopes_null_one_param, t = \
+            train_and_test_all_subjs(sp_param, sparam_dir)
+        ctf_slopes_all_params[sp_param] = ctf_slopes_one_param
+        ctf_slopes_null_all_params[sp_param] = ctf_slopes_null_one_param
         if verbose:
             print(f'Fit IEMs for {sp_param} in {time.time() - start:.2f} s')
-    return ctf_slopes_all_params, ctf_slopes_null_all_params, t_arrays
+    return ctf_slopes_all_params, ctf_slopes_null_all_params, t
 
 
 def plot_ctf_slope_time_courses(
-        ctf_slopes, ctf_slopes_null, t_arrays,
+        ctf_slopes_all_params, ctf_slopes_null_all_params, t,
         subjects_by_task=params.SUBJECTS_BY_TASK, fig_dir=params.FIG_DIR,
         task_timings=params.TASK_TIMINGS):
     """Plot CTF slope time courses for total power and parameters from spectral
@@ -51,17 +51,19 @@ def plot_ctf_slope_time_courses(
     for task_num, (experiment, _) in enumerate(subjects_by_task):
         ctf_slopes_fname = os.path.join(
             fig_dir, f'ctf_slopes_{experiment}_task{task_num}.png')
-        ctf_slopes = {k: v[task_num] for k, v in ctf_slopes.items()}
+        ctf_slopes_all_params = {
+            k: v[task_num] for k, v in ctf_slopes_all_params.items()}
         ctf_slopes_shuffled = {
-            k: v[task_num] for k, v in ctf_slopes_null.items()}
+            k: v[task_num] for k, v in ctf_slopes_null_all_params.items()}
         plot_ctf_slope(
-            ctf_slopes, t_arrays[task_num], task_num,
+            ctf_slopes_all_params, t[task_num], task_num,
             task_timings=task_timings[task_num],
             ctf_slopes_shuffled=ctf_slopes_shuffled, palette='Set1',
             save_fname=ctf_slopes_fname)
 
 
-def plot_paired_ttests(ctf_slopes, ctf_slopes_null, t_arrays):
+def plot_paired_ttests(
+        ctf_slopes_all_params, ctf_slopes_null_all_params, t):
     """Plot paired t-tests of CTF slopes for desired parameters from spectral
     parameterization model."""
     # Plot paired t-tests of CTF slopes for the aperiodic exponent in first
@@ -69,16 +71,16 @@ def plot_paired_ttests(ctf_slopes, ctf_slopes_null, t_arrays):
     exp_ctf_slope_fname = f'{params.FIG_DIR}/exp_ctf_slope_paired_ttest.png'
     cmap = plt.get_cmap('Paired')
     plot_ctf_slope_paired_ttest(
-        ctf_slopes['exponent'], t_arrays, (0.0, 0.4),
-        ctf_slopes_shuffled=ctf_slopes_null['exponent'],
+        ctf_slopes_all_params['exponent'], t, (0.0, 0.4),
+        ctf_slopes_shuffled=ctf_slopes_null_all_params['exponent'],
         palette=(cmap(3), cmap(2)), save_fname=exp_ctf_slope_fname)
 
     # Plot paired t-tests of CTF slopes for alpha oscillatory power in WM
     pw_ctf_slope_fname = f'{params.FIG_DIR}/pw_ctf_slope_paired_ttest.png'
     plot_ctf_slope_paired_ttest(
-        ctf_slopes['PW'], t_arrays, 'WM',
-        ctf_slopes_shuffled=ctf_slopes_null['PW'], palette=(cmap(1), cmap(0)),
-        save_fname=pw_ctf_slope_fname)
+        ctf_slopes_all_params['PW'], t, 'WM',
+        ctf_slopes_shuffled=ctf_slopes_null_all_params['PW'],
+        palette=(cmap(1), cmap(0)), save_fname=pw_ctf_slope_fname)
 
 
 if __name__ == '__main__':
