7e5f487a8c0be014cba6fdf6c7f4ca2fe0cfb9a1
Andrew Bender <andrew.bender710@gmail.com>
2024-02-07 10:42:50 -0800

    Only plot alpha total power, oscillatory power, and exponent for ctf slope fig

--- code/spec_param_iem.py
+++ code/spec_param_iem.py
@@ -3,6 +3,7 @@ using same inverted encoding model (IEM) and EEG data as Foster and colleagues
 (https://pubmed.ncbi.nlm.nih.gov/26467522/."""
 
 # Import necessary modules
+import matplotlib.pyplot as plt
 from train_and_test_iem import fit_iem_desired_params
 from plot_iem_results import (
     plot_ctf_slope_time_courses,
@@ -11,25 +12,33 @@ from plot_iem_results import (
 )
 
 if __name__ == "__main__":
-    # Fit IEM for total power and all parameters from spectral parameterization
-    # model
-    ctf_slopes, ctf_slopes_null, t_arrays = fit_iem_desired_params(
-        verbose=False
-    )
-
-    # Plot CTF slope time courses for all parameters from spectral
+    # Plot CTF slope time courses for desired parameters from spectral
     # parameterization model
+    ctf_slopes, ctf_slopes_null, t_arrays = fit_iem_desired_params(
+        sp_params=["total_power", "linOscAUC", "exponent"], verbose=False
+    )
+    param_names = {
+        "total_power": "Alpha total power",
+        "linOscAUC": "Alpha oscillatory power",
+        "exponent": "Aperiodic exponent",
+    }
     plot_ctf_slope_time_courses(
         ctf_slopes,
         t_arrays,
         ctf_slopes_contrast=ctf_slopes_null,
         title="All parameters",
+        palettes=["Set1"],
+        param_names=param_names,
+        plt_errorbars=True,
     )
-
-    # Plot CTF slope time courses for relevant comparisons of parameters from
-    # spectral parameterization model
-    compare_params_ctf_time_courses(ctf_slopes, ctf_slopes_null, t_arrays)
 
     # Plot paired t-tests of CTF slopes for parameters from spectral
     # parameterization model
     plot_paired_ttests(ctf_slopes, ctf_slopes_null, t_arrays)
+
+    # Plot CTF slope time courses for relevant comparisons of parameters from
+    # spectral parameterization model
+    ctf_slopes, ctf_slopes_null, t_arrays = fit_iem_desired_params(
+        sp_params="all", verbose=False
+    )
+    compare_params_ctf_time_courses(ctf_slopes, ctf_slopes_null, t_arrays)
