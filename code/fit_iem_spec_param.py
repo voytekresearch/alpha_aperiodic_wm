5ce68b8d5cef34f8ec9d254d04b1de7ba15da790
Andrew Bender <andrew.bender710@gmail.com>
2024-11-13 15:47:43 -0800

    Simplify fitting scripts

--- code/fit_iem_spec_param.py
+++ code/fit_iem_spec_param.py
@@ -5,7 +5,6 @@
 # Import necessary modules
 import numpy as np
 from train_and_test_model import fit_model_desired_params
-from plot_model_fits import plot_model_fit_time_courses
 import params
 
 if __name__ == "__main__":
@@ -17,18 +16,4 @@ if __name__ == "__main__":
     ctf_slopes, t_arrays = fit_model_desired_params(
         sp_params=["total_power", "linOscAUC", "exponent"],
         verbose=False,
-    )
-    param_names = {
-        "total_power": "Alpha total power",
-        "linOscAUC": "Alpha oscillatory power",
-        "exponent": "Aperiodic exponent",
-    }
-    plot_model_fit_time_courses(
-        ctf_slopes,
-        t_arrays,
-        title="All parameters",
-        palettes=["Set1"],
-        param_names=param_names,
-        plt_errorbars=True,
-        name="iem",
     )
